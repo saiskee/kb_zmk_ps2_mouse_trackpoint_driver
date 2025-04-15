4d75e84772533e068a452c5d4190ac71291c2433
Sai Ekbote <sekbote@hubspot.com>
2025-04-15 18:10:35 -0400

    try reading xyavg_factor

--- src/drivers/input/input_mouse_ps2.c
+++ src/drivers/input/input_mouse_ps2.c
@@ -113,7 +113,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #define MOUSE_PS2_CMD_TP_SET_VALUE6_UPPER_PLATEAU_SPEED_DEFAULT 0x61
 
 #define MOUSE_PS2_ST_TP_Z_FORCE "tp_z_force"
-#define MOUSE_PS2_CMD_TP_GET_Z_FORCE "\xe2\x80"
+#define MOUSE_PS2_CMD_TP_GET_Z_FORCE "\xe2\x80\x46"
 #define MOUSE_PS2_CMD_TP_GET_Z_FORCE_RESP_LEN 1
 
 #define MOUSE_PS2_ST_TP_PTS_THRESHOLD "tp_pts_threshold"
@@ -1601,14 +1601,14 @@ int zmk_mouse_ps2_tp_value6_upper_plateau_speed_change(int amount) {
 int zmk_mouse_ps2_tp_z_force_get(uint8_t *z_force) {
 
     struct zmk_mouse_ps2_send_cmd_resp resp = zmk_mouse_ps2_send_cmd(
-        MOUSE_PS2_CMD_TP_GET_Z_FORCE, sizeof(MOUSE_PS2_CMD_TP_GET_Z_FORCE), "\x3e", MOUSE_PS2_CMD_TP_GET_Z_FORCE_RESP_LEN, true);
+        MOUSE_PS2_CMD_TP_GET_Z_FORCE, sizeof(MOUSE_PS2_CMD_TP_GET_Z_FORCE), NULL, MOUSE_PS2_CMD_TP_GET_Z_FORCE_RESP_LEN, true);
     if (resp.err) {
         LOG_ERR("Could not get Z-axis force: %s (err: %d)", resp.err_msg, resp.err);
         return resp.err;
     }
 
     uint8_t zforce = resp.resp_buffer[0];
-    LOG_DBG("Trackpoint Z-force is %d", zforce);
+    LOG_DBG("Trackpoint xyzavg is %d", zforce);
     return 0;
 }
 
