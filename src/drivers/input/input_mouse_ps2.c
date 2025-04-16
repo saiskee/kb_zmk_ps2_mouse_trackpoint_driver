cc97880bed573e5edc74f5c20e168b81c34dab4b
Sai Ekbote <sekbote@hubspot.com>
2025-04-15 20:00:38 -0400

    change to 2f

--- src/drivers/input/input_mouse_ps2.c
+++ src/drivers/input/input_mouse_ps2.c
@@ -877,7 +877,7 @@ void zmk_mouse_ps2_activity_move_mouse(int16_t mov_x, int16_t mov_y) {
                 char cmd[3] = { 0xE2, 0x80, current_ram_addr };
 
                 struct zmk_mouse_ps2_send_cmd_resp resp = zmk_mouse_ps2_send_cmd(
-                    cmd, 3, NULL, 1, true);
+                    "\xe2\x80\x2f", 3, NULL, 1, true);
 
                 if (resp.err) {
                     LOG_WRN("RAM[0x%02X] read failed: %s (err: %d)",
